598833b6a5d420754244de898e6c50be9ce88b15
niwatoro <girlwithskyhairpin@gmail.com>
2023-12-02 18:49:53 +0900

    Add styles to Header component and import Montserrat font

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
+import styles from "@/styles/components/Header/Header.module.css";
 import { Navbar, NavbarBrand } from "@nextui-org/react";
 
 export const Header = () => {
   return (
     <Navbar isBordered={true}>
-      <NavbarBrand>RippleEVM</NavbarBrand>
+      <NavbarBrand>
+        <div className={styles.container}>RippleEVM</div>
+      </NavbarBrand>
     </Navbar>
   );
 };
