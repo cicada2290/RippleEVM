07d58e6765fdaacf5e96160861d10e2098ac5428
niwatoro <girlwithskyhairpin@gmail.com>
2023-12-12 00:20:27 +0900

    公開鍵の取得時にエラー時にnullを返す

--- src/scripts/sections/evm/get-evm-public-key.ts
+++ src/scripts/sections/evm/get-evm-public-key.ts
@@ -6,32 +6,37 @@ import {
 } from "ethers";
 
 export const getEvmPublicKey = async (evmAddress: string) => {
-  for (const network of NETWORKS) {
-    if (network.type !== "evm") {
-      continue;
-    }
-
-    const transactionResponse = await fetch(
-      `/api/evm/get-transactions?chainId=${network.chainId}&address=${evmAddress}`,
-    );
-    if (!transactionResponse.ok) {
-      continue;
-    }
-
-    const transactions = (await transactionResponse.json()).data;
-    const provider = new JsonRpcProvider(network.url);
-    for (const transaction of transactions) {
-      const tx = await provider.getTransaction(transaction.hash);
-      if (!tx || tx.from !== evmAddress) {
+  try {
+    for (const network of NETWORKS) {
+      if (network.type !== "evm") {
         continue;
       }
 
-      const txObj = EvmTransaction.from(tx);
-      if (!txObj.fromPublicKey) {
+      const transactionResponse = await fetch(
+        `/api/evm/get-transactions?chainId=${network.chainId}&address=${evmAddress}`,
+      );
+      if (!transactionResponse.ok) {
         continue;
       }
-      return SigningKey.computePublicKey(txObj.fromPublicKey) ?? null;
+
+      const transactions = (await transactionResponse.json()).data;
+      const provider = new JsonRpcProvider(network.url);
+      for (const transaction of transactions) {
+        const tx = await provider.getTransaction(transaction.hash);
+        if (!tx || tx.from !== evmAddress) {
+          continue;
+        }
+
+        const txObj = EvmTransaction.from(tx);
+        if (!txObj.fromPublicKey) {
+          continue;
+        }
+        return SigningKey.computePublicKey(txObj.fromPublicKey) ?? null;
+      }
     }
+    return null;
+  }catch(e:any){
+    console.error(e.message)
+    return null;
   }
-  return null;
 };
